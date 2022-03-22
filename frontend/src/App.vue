223b43fa0e41936da0868f428638e2aafac9e485
Piotr Orczykowski <orczykowski.peter@gmail.com>
2022-03-22 16:24:01 +0100

    Create component for welcome message

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <div id="app">
     <div id="left">
-      <div id="graphic">
-        <img src="@/assets/welcomeImage.jpeg" id="image">
-      </div>
-      <div id="message">
-        <h2>Welcome back!</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Proin rhoncus quam scelerisque, ultrices velit non, interdum massa.
-        </p>
-      </div>
+      <WelcomeMessage />
     </div>
     <div id="right">
       <router-view />
@@ -18,9 +10,12 @@
 </template>
 
 <script>
+import WelcomeMessage from './components/WelcomeMessage.vue'
 
 export default {
-
+  components: {
+    WelcomeMessage
+  }
 }
 </script>
 
@@ -28,7 +23,6 @@ export default {
   * {
     margin: 0;
     padding: 0;
-
   }
 
   #app {
@@ -43,28 +37,6 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: center;
-  }
-
-  #image {
-    width: 35vw;
-    height: auto;
-    margin-left: calc((45vw - 35vw) / 2 );
-  }
-
-  #message {
-    text-align: center;
-    color: white;
-  }
-
-  #message h2 {
-    font-size: 2em;
-    color: white;
-    padding-top: 1em;
-  }
-
-  #message p {
-    font-size: 1.2em;
-    padding: 1em;
   }
 
   #right {
